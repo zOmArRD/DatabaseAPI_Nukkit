0949a45f9c5b4c910c576f6d49d50a2b42fc7eff
zOmArRD <omarsena25@gmail.com>
2023-09-13 01:15:56 -0400

    Initial commit

---
+++ src/main/java/dev/zomarrd/dbapi/PluginLoader.java
@@ -0,0 +1,17 @@
+/*
+ * Created by IntelliJ Idea
+ *
+ * User: zOmArRD
+ * Date: 12/9/2023
+ *
+ * Copyright (c) 2023. OMY TECHNOLOGY by <dev@zomarrd.me>
+ */
+
+package dev.zomarrd.dbapi;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class PluginLoader extends PluginBase {
+    @Override
+    public void onEnable() {}
+}
